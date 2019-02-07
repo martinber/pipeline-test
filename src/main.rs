21751534539021b827e6215f0d655e4a77e7c96d
Martin Bernardi (PC) <martinber.710@gmail.com>
2019-02-07 14:34:06 -0300

    get modified

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate hound;
 
+
+// https://gist.github.com/kevincox/019a0a4d1024e5bddd4be1cbe88fb2bc
+
+use std::iter::Iterator;
+use std::collections::VecDeque;
+
 fn main() {
 
     let mut a = MyIter::new(vec![1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]);
 
-    println!("{:?}", a.get(3));
+
     let b = duplicar(a);
     let c = duplicar(b);
 
@@ -13,30 +19,48 @@ fn main() {
 
 }
 
-fn duplicar(entrada: impl std::iter::Iterator<Item=f32>) -> impl std::iter::Iterator<Item=f32> {
+fn duplicar(entrada: impl Iterator<Item=f32>) -> impl Iterator<Item=f32> {
 
     entrada.map(|x| x * 2.)
 }
 
+/// The front has the oldest value. The back has the newest value
 struct MyIter {
-    buffer: Vec<f32>,
+    buffer: VecDeque<f32>,
 }
 
 impl MyIter {
-    pub fn new(buffer: Vec<f32>) -> MyIter {
-        MyIter { buffer }
+    pub fn new(v: Vec<f32>) -> MyIter {
+        MyIter { buffer: VecDeque::from(v) }
     }
 
-    pub fn get(&mut self, n: usize) -> &[f32] {
-        &self.buffer[self.buffer.len() - n..]
+    pub fn get(&mut self, mut index: i32) -> &f32 {
+        if index < 0 {
+            // Negative indices count from back
+            // index = match self.buffer.len().checked_add(index) {
+                // Some(i) => i, // Return the index right away
+                // None => { // len-index < 0, so we need to ask for more values
+                    // panic!("End reached");
+                // }
+            // }
+
+            // Ask for more values
+            if self.buffer.len() as i32 + index < 0 {
+                panic!("End reached");
+            }
+            index = self.buffer.len() as i32 + index;
+        }
+        assert!(index >= 0);
+        self.buffer.get(index as usize).unwrap()
     }
 }
 
-impl std::iter::Iterator for MyIter {
+
+impl Iterator for MyIter {
     type Item = f32;
 
     fn next(&mut self) -> Option<f32> {
-        self.buffer.pop()
+        self.buffer.pop_back()
     }
 }
 
