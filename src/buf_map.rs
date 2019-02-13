24dd1e353dde7c54277efdfcf56d64937ccc3387
Martin Bernardi (PC) <martinber.710@gmail.com>
2019-02-13 14:19:33 -0300

    Movido BufMap a modulo

---
+++ src/buf_map.rs
@@ -0,0 +1,125 @@
+use std::iter::Iterator;
+use std::collections::VecDeque;
+
+/// Buffer accessible when using `buf_map()`.
+///
+/// It's a wrapper around `VecDeque` that also owns a `Iterator`.
+/// When calling `get()` this buffer takes a value from the `Iterator`.
+///
+/// The front of the `VecDeque` has the newest value. The back has the oldest
+/// value.
+pub struct Buffer
+{
+    source: Box<Iterator<Item=f32>>,
+    pub deque: VecDeque<f32>,
+}
+
+impl Buffer
+{
+    pub fn new(iter: impl Iterator<Item=f32> + 'static) -> Buffer {
+        Buffer {
+            source: Box::new(iter),
+            deque: VecDeque::new(),
+        }
+    }
+
+    /// Fills the buffer to reach given length. Returns if succesful.
+    pub fn fill(&mut self, length: usize) -> bool {
+        let mut success = true;
+        while self.deque.len() < length {
+            success = self.pull()
+        }
+        return success;
+    }
+
+    /// Pull a value from the iterator. Returns if able to get value.
+    pub fn pull(&mut self) -> bool {
+        match self.source.next() {
+            Some(v) => {
+                self.deque.push_front(v);
+                true
+            },
+            None => false,
+        }
+    }
+
+    /// Pop oldest value on buffer.
+    pub fn pop(&mut self) -> Option<f32> {
+        self.deque.pop_back()
+    }
+}
+
+// Implement indexing on `Buffer`
+
+impl std::ops::Index<i32> for Buffer {
+    type Output = f32;
+
+    fn index(&self, mut index: i32) -> &f32 {
+        if index < 0 {
+            index = self.deque.len() as i32 + index;
+        }
+        if index < 0 {
+            panic!("Negative buffer index out of bounds");
+        }
+        self.deque.get(index as usize)
+            .expect("Positive buffer index out of bounds")
+    }
+}
+
+
+/// An iterator that maps the values of an `Iterator` using a `Buffer`.
+pub struct BufMap<F>
+    where F: Fn(&Buffer) -> f32
+{
+    buffer: Buffer,
+    function: F,
+}
+
+impl<F> BufMap<F>
+    where F: Fn(&Buffer) -> f32
+{
+    pub fn new(iter: impl Iterator<Item=f32> + 'static, window: usize, function: F) -> BufMap<F>
+    {
+        let mut iter = BufMap {
+            buffer: Buffer::new(iter),
+            function,
+        };
+        // Because I pull before next()
+        if ! iter.buffer.fill(window - 1) {
+            panic!("Iterator too short to fill BufMap Buffer");
+        }
+        iter
+    }
+}
+
+// Implement `std::Iterator` on `BufMap`
+
+impl<F> Iterator for BufMap<F>
+    where F: Fn(&Buffer) -> f32
+{
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        if self.buffer.pull() {
+            let result = (self.function)(&self.buffer);
+            self.buffer.pop();
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
+
+// Add method `buf_map()` to `std::iter::Iterator`
+
+pub trait BufMapExt: Iterator {
+    fn buf_map<F>(self, window: usize, function: F) -> BufMap<F>
+        where F: Fn(&Buffer) -> f32,
+              Self: Iterator<Item=f32> + 'static + Sized,
+    {
+        BufMap::new(self, window, function)
+    }
+}
+
+impl<I: Iterator> BufMapExt for I {}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,36 @@
 extern crate hound;
 
+mod buf_map;
 
-// https://gist.github.com/kevincox/019a0a4d1024e5bddd4be1cbe88fb2bc
-
-use std::iter::Iterator;
-use std::collections::VecDeque;
+use buf_map::{Buffer, BufMapExt};
 
 fn main() {
 
-    test()
+    // test()
 
     // bench()
 
-    // bench_iter()
+    bench_buf()
 
 }
 
 fn test() {
 
-    let source = (1..).map(|x| x as f32);
+    let result: Vec<f32> = (1..)
+        .map(|x| x as f32)
+        .map(|x| x * 2.)
+        .buf_map(1, |buf| {
+            buf[-1] * 2.
+        })
+        .buf_map(3, |buf| {
+            let mut sum: f32 = 0_f32;
+            for j in 0..3 {
+                sum += buf[j] * 0.2;
+            }
+            sum
+        })
+        .take(100).collect();
 
-    let first_map = MyIter::new(source, 1, |buf| {
-        buf.get(-1) * 2.
-    });
-
-    let second_map = MyIter::new(first_map, 3, |buf| {
-        let mut sum: f32 = 0_f32;
-        for j in 0..3 {
-            sum += buf.get(-(j as i32)) * 0.2;
-        }
-        sum
-    });
-
-    let result: Vec<f32> = second_map.take(100).collect();
     println!("{:?}", result);
 }
 
@@ -60,139 +58,14 @@ fn bench() {
     // let c: Vec<f32> = b.collect();
 }
 
-fn bench_iter() {
+fn bench_buf() {
 
-    let origin = (1..).map(|x| x as f32).take(100000000);
-
-
-    let b = MyIter::new(origin, 3, |buf| filter_2(buf, &[1., 1., 1.]));
-
-    let c: Vec<f32> = b.collect();
+    let a: Vec<f32> = (1..)
+        .map(|x| x as f32)
+        .take(100000000)
+        .buf_map(3, |buf| filter(buf, &[1., 1., 1.]))
+        .collect();
 }
-
-pub struct Buffer
-{
-    origin: Box<Iterator<Item=f32>>,
-    pub deque: VecDeque<f32>,
-}
-
-impl Buffer
-{
-    pub fn new(v: impl Iterator<Item=f32> + 'static) -> Buffer {
-        Buffer {
-            origin: Box::new(v),
-            deque: VecDeque::new(),
-        }
-    }
-
-    // pub fn get(&mut self, mut index: i32) -> f32 {
-        // if index < 0 {
-//
-            // // Ask for more values
-            // if self.deque.len() as i32 + index < 0 {
-                // while self.deque.len() as i32 + index < 0 {
-                    // self.deque.push_front(self.origin.next().unwrap());
-                // }
-            // }
-            // index = self.deque.len() as i32 + index;
-        // }
-        // assert!(index >= 0);
-        // *self.deque.get(index as usize).unwrap()
-    // }
-
-    pub fn get_neg(&self, mut index: usize) -> f32 {
-        index = self.deque.len().checked_sub(index + 1).unwrap();
-        *self.deque.get(index)
-            .expect("Out of bounds")
-    }
-
-    pub fn get(&self, mut index: i32) -> f32 {
-        if index < 0 {
-            index = self.deque.len() as i32 + index;
-        }
-        if index < 0 {
-            panic!("Negative buffer index out of bounds");
-        }
-        *self.deque.get(index as usize)
-            .expect("Positive buffer index out of bounds")
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item=&f32> + '_ {
-        self.deque.iter().rev()
-    }
-
-    /// Fills the buffer to reach given length. Returns if succesful.
-    pub fn fill(&mut self, length: usize) -> bool {
-        let mut success = true;
-        while self.deque.len() < length {
-            success = self.pull()
-        }
-        return success;
-    }
-
-    /// Pull a value from the iterator. Returns if able to get value.
-    pub fn pull(&mut self) -> bool {
-        match self.origin.next() {
-            Some(v) => {
-                self.deque.push_front(v);
-                true
-            },
-            None => false,
-        }
-    }
-
-    pub fn pop(&mut self) -> Option<f32> {
-        self.deque.pop_back()
-    }
-}
-
-
-/// The front has the oldest value. The back has the newest value
-struct MyIter<F>
-    where F: Fn(&Buffer) -> f32
-{
-    buffer: Buffer,
-    function: F,
-    window: usize,
-}
-
-impl<F> MyIter<F>
-    where F: Fn(&Buffer) -> f32
-{
-    pub fn new(iter: impl Iterator<Item=f32> + 'static, window: usize, function: F) -> MyIter<F>
-    {
-        let mut iter = MyIter {
-            buffer: Buffer::new(iter),
-            function,
-            // function: |buf: Buffer<Iter>| buf.deque.pop_back(),
-            window,
-        };
-        // Because I pull before next()
-        if ! iter.buffer.fill(window - 1) {
-            panic!("AA");
-        }
-        iter
-    }
-}
-
-impl<F> Iterator for MyIter<F>
-    where F: Fn(&Buffer) -> f32
-{
-    type Item = f32;
-
-    fn next(&mut self) -> Option<f32> {
-        if self.buffer.pull() {
-            let result = (self.function)(&self.buffer);
-            self.buffer.pop();
-            Some(result)
-        } else {
-            None
-        }
-    }
-}
-
-
-
 
 /// Filter a signal.
 pub fn filter(
@@ -202,20 +75,7 @@ pub fn filter(
 
     let mut sum: f32 = 0_f32;
     for j in 0..coeff.len() {
-        sum += buffer.get(-(j as i32)) * coeff[j];
-    }
-    sum
-}
-
-/// Filter a signal.
-pub fn filter_2(
-    buffer: &Buffer,
-    coeff: &[f32],
-) -> f32 {
-
-    let mut sum: f32 = 0_f32;
-    for j in 0..coeff.len() {
-        sum += buffer.get_neg(j) * coeff[j];
+        sum += buffer[-(j as i32)] * coeff[j];
     }
     sum
 }
